740d435c49eab63860616c06ebd98907822a7f34
Sensess <SensessYentzer@outlook.com>
2022-07-13 19:05:56 -0400

    update

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -35,8 +35,7 @@ const typeDefs = gql`
 
   type Mutation {
     login(email: String!, password: String!): User
-    addUser(username: String!, email: String! password: String!): User
-    
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
